cd38e0bad3ef61145fe8820f863a3d47964635e7
dylanyborg <58005999+dylanyborg@users.noreply.github.com>
2022-05-27 16:27:40 -0400

    idk

---
+++ resources/js/components/LikeButton.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <button type="submit" value="" id="likedSongButton" @click="likeSong">
+            <img id="likedSongImage" src="">
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            likeSong() {
+                console.log("like song button clicked");
+            }
+        }
+    }
+</script>
