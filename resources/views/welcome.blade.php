25b3d17abc8fca1ec54ad9f2946ad9e14361e138
dylanyborg <58005999+dylanyborg@users.noreply.github.com>
2022-05-27 20:24:34 -0400

    fail2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="/js/app.js" defer></script>
     </head>
 
     <body class="font-sans antialiased">
